d451480934ac6fdbfac7f273767d23cfa6079a93
WeronikaBL <weronika@datenexperten.com>
2021-11-03 23:53:46 +0100

    feat: add bootstrap template

--- zvrot/src/App.js
+++ zvrot/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <header className="mb-auto">
+          <div>
+            <h3 className="float-md-start mb-0">Zvrot</h3>
+            <nav className="nav nav-masthead justify-content-center float-md-end">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+              <a className="nav-link" href="#">Features</a>
+              <a className="nav-link" href="#">Contact</a>
+            </nav>
+          </div>
+        </header>
+
+        <main className="px-3">
+          <h1>Cover your page.</h1>
+          <p className="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+          <p className="lead">
+            <a href="#" className="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+          </p>
+        </main>
+
+        <footer className="mt-auto text-white-50">
+          <p>Cover template for <a href="https://getbootstrap.com/" className="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" className="text-white">@mdo</a>.</p>
+        </footer>
+      </>
   );
 }
 
