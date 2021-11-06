39de424e48476fb7519a45058b8d178352f69057
WeronikaBL <weronika@datenexperten.com>
2021-11-06 13:44:55 +0100

    fix: nav items position

--- zvrot/src/components/Header/index.js
+++ zvrot/src/components/Header/index.js
@@ -3,22 +3,24 @@ import { Navbar, Container, Nav } from 'react-bootstrap'
 
 export default function Header() {
     return (
-        <Navbar bg="white" expand="md">
-            <Container>
-                <Navbar.Brand href="#home">
-                <img src="/assets/logo.png" alt="zvrot logo" className="img-fluid App-logo"></img>
+        <header>
+            <Navbar bg="white" expand="md">
+                <Container className="align-items-end">
+                    <Navbar.Brand href="#home">
+                    <img src="/assets/logo.png" alt="zvrot logo" className="img-fluid App-logo"></img>
 
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav" className="flex-grow-0">
-                    <Nav className="me-auto justify-content-end">
-                        <Nav.Link href="#home">Ratujemy twoje straty!</Nav.Link>
-                        <Nav.Link href="#home">Zvrot</Nav.Link>
-                        <Nav.Link href="#login">Logowanie</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav" className="flex-grow-0">
+                        <Nav className="me-auto justify-content-end">
+                            <Nav.Link href="#home" className="pb-0 mt-auto">Ratujemy twoje straty!</Nav.Link>
+                            <Nav.Link href="#home" className="pb-0 mt-auto">Zvrot</Nav.Link>
+                            <Nav.Link href="#login" className="pb-0 mt-auto">Logowanie</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
 
     )
 }
