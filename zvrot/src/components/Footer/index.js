46023514618ca2d59c17cfdf883771acc0110663
WeronikaBL <weronika@datenexperten.com>
2021-11-06 13:29:44 +0100

    fix: prepare for responsive

--- zvrot/src/components/Footer/index.js
+++ zvrot/src/components/Footer/index.js
@@ -1,19 +1,29 @@
 import React from 'react'
-import { Nav } from 'react-bootstrap';
+import { Nav, Col, Row, Container } from 'react-bootstrap';
 import FooterItem from './FooterItem'
 import FooterSocialItems from './FooterSocialItems';
 function Footer() {
     const items = [ "Kontakt", "Polityka Prywatności", "Cookies", "Regulamin", "O nas", "Reklama"];
     
     return (
-        <footer className="mt-auto bd-footer p-3 p-md-5 mt-5 bg-light text-center text-sm-left d-flex justify-content-between">
-                <Nav className="navbar justify-content-start">
-                    {items.map((item, i) => {
-                        return <FooterItem key={item} text={item} />
-                    })}
-                </Nav>
-                <FooterSocialItems />
+        
+        <footer className="mt-auto bd-footer bg-light text-center text-sm-left">
+            <Container>
+                <Row className="pt-3">
+                    <Col xs={12} md={6}>
+                        <Nav className="navbar justify-content-start align-content-start flex-wrap">
+                            {items.map((item, i) => {
+                                return <FooterItem key={item} text={item} />
+                            })}
+                        </Nav>
+                    </Col>
+                    <Col xs={12} md={6}>
+                        <FooterSocialItems />
+                    </Col>
+                </Row>
+            </Container>
         </footer>
+        
     )
 }
 
